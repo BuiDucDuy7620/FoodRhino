2ddb229c0d6d76ac8d5096dedc8ddd1a96e495f8
BuiDuy76 <88266315+BuiDuy76@users.noreply.github.com>
2023-02-16 11:57:05 +0700

    ud cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = require("./router/router.js");
 const mongoose = require("mongoose");
 const app = express();
-
+const cors=require('cors')
 const db = "mongodb+srv://buiducduy:Buiducduy%4007062000@cluster0.fugtbzu.mongodb.net/foodRhino";
 
 const PORT = process.env.PORT || 3000;
@@ -10,6 +10,16 @@ mongoose.connect(db, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 },()=>{console.log('ket noi db thanh cong');});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+
+//     res.header("Access-Control-Allow-Headers", "Content-Type");
+//     next();
+//   });
+app.use(cors())
+app.options('*',cors())
+
 app.use(express.json());
 app.use(
     express.urlencoded({
--- router/bookADemoRouter.js
+++ router/bookADemoRouter.js
@@ -5,8 +5,8 @@ bookADemoRouter.post('/book-a-demo',bookADemoController.postBookADemo)
 
 bookADemoRouter.get('/getAllBookADemo',bookADemoController.getAllBookADemo)
 bookADemoRouter.get('/getBookADemoById/:id',bookADemoController.getBookADemoById)
-bookADemoRouter.get('/exportBookADemoById/:id',bookADemoController.exportBookADemoById)
-bookADemoRouter.get('/exportAllBookADemo',bookADemoController.exportAllBookADemo)
+bookADemoRouter.get('/exportExcelBookADemoById/:id',bookADemoController.exportBookADemoById)
+bookADemoRouter.get('/exportExcelAllBookADemo',bookADemoController.exportAllBookADemo)
 
 bookADemoRouter.get('/exportPDFListBookADemo',bookADemoController.exportPDFListBookADemo)
 
